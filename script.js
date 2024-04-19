3bafc2931ebb759d241f8912923da6c1dd12c9a0
Mylifeaskyara <kyara.nonaka@gmail.com>
2024-04-19 11:38:42 +0900

    animation

--- script.js
+++ script.js
@@ -8,17 +8,46 @@ $(window).on('load',function(){
 function fadeAnime(){
 
   //ふわっと動くきっかけのクラス名と動きのクラス名の設定
-  $('.fadeUpTrigger').each(function(){ 
-  　　var elemPos = $(this).offset().top-50; 
+  $('.fadeUpTrigger').each(function(){ //fadeInUpTriggerというクラス名が
+  　　var elemPos = $(this).offset().top-10; //要素より、50px上の
   　　var scroll = $(window).scrollTop();
   　　var windowHeight = $(window).height();
   　　if (scroll >= elemPos - windowHeight){
   　　$(this).addClass('fadeUp');
+  　　// 画面内に入ったらfadeInというクラス名を追記
   　　}else{
   　　　$(this).removeClass('fadeUp');
+  　　// 画面外に出たらfadeInというクラス名を外す
   　　}
   　　});
-}
+  
+  //関数でまとめることでこの後に違う動きを追加することが出来ます
+  $('.fadeDownTrigger').each(function(){ //fadeInDownTriggerというクラス名が
+  　　var elemPos = $(this).offset().top-50; //要素より、50px上の
+  　　var scroll = $(window).scrollTop();
+  　　var windowHeight = $(window).height();
+  　　if (scroll >= elemPos - windowHeight){
+  　　$(this).addClass('fadeDown');
+  　　// 画面内に入ったらfadeDownというクラス名を追記
+  　　}else{
+  　　　$(this).removeClass('fadeDown');
+  　　// 画面外に出たらfadeDownというクラス名を外す
+  　　}
+  　　});
+  
+  
+  }//ここまでふわっと動くきっかけのクラス名と動きのクラス名の設定
+  
+  // 画面をスクロールをしたら動かしたい場合の記述
+    $(window).scroll(function (){
+      fadeAnime();/* アニメーション用の関数を呼ぶ*/
+    });// ここまで画面をスクロールをしたら動かしたい場合の記述
+  
+    // 画面が読み込まれたらすぐに動かしたい場合の記述
+  $(window).on('load', function(){
+    fadeAnime();/* アニメーション用の関数を呼ぶ*/
+  });// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+
 
 // Hamburger Menu 
 document.querySelector('.hamburger').addEventListener('click',function(){
@@ -58,10 +87,6 @@ document.querySelector('.hamburger').addEventListener('click',function(){
   PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
   });
   
-  // ページが読み込まれたらすぐに動かしたい場合の記述
-  $(window).on('load', function () {
-  PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
-  });
   
   // #page-topをクリックした際の設定
   $('#page-top').click(function () {
